422e81eb7b81e52cadd634f1a4d029d53c83409e
MattHowdy <mate.hodi9@gmail.com>
2018-06-27 13:48:18 +0200

    update and refactor main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    seq = list(range(1,22))
+    seq = list(range(1,13))
     return render_template('index.html', seq=seq)
 
 
@@ -14,13 +14,14 @@ def game():
     number = int(request.form.get('comp_select'))*2
     cards = list(range(1, int(number)+1))
 
-    return render_template('the-game.html', cards=cards)
+    seq = list(range(1, 13))
+    return render_template('the-game.html', cards=cards, seq=seq)
 
 
 
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
-        port=8000,
+        port=8001,
         debug=True
     )
