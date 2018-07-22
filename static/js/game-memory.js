eaa3945df75d90f1fb02253828bff4bfb4db760d
MattHowdy <mate.hodi9@gmail.com>
2018-07-22 15:44:04 +0200

    refactoring

--- static/js/game-memory.js
+++ static/js/game-memory.js
@@ -2,7 +2,7 @@
 // start the memory game
 function main( ) {
     let deckArray = ['1.jpg', '2.jpg', "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", '12.jpg'];
-    let cards = document.getElementsByClassName('cards');
+    let cards = $('.cards');
     let numberOfCards = cards.length;
 
     let shuffledCards = shuffle(deckArray, numberOfCards);
@@ -40,53 +40,46 @@ function shuffle(array, card_number) {
 
 //tags cards
 function taggingCards(shuffledCards){
-    let cards = document.getElementsByClassName('cards');
+    let cards = $('.cards');
     for (let i = 0; i < cards.length; i++) {
         let card = cards[i];
-        card.addEventListener('click',function(){ isThereaMatch(shuffledCards);});
-            }
+        card.addEventListener('click', function (event){
+            isThereaMatch(shuffledCards, event, i);
+            });
         }
+    }
 
 
 
     var openedCards = [];
-    var openCardsID = [];
     var flipCounter = 0;
 
 //check matched cards
-function isThereaMatch(shuffledcards) {
+function isThereaMatch(shuffledcards, event, index) {
     let deck = shuffledcards;
 
-    let imgToChange = document.getElementById(event.target.id);
+    let card = event.currentTarget;
 
     if (openedCards.length < 2) {
-        imgToChange.innerHTML = `<img class="${ deck[event.target.id-1] }" src="/static/img/${ deck[event.target.id-1] }" width="100" height="100"></img>`;
-        // let changedImg = document.getElementById(event.target.id);
+        card.innerHTML = `<img class="${ deck[index] }" src="/static/img/${ deck[index] }" width="100" height="100"></img>`;
         if(openedCards.length === 0){
-            openedCards.push(imgToChange.childNodes);
-            openCardsID.push(event.target.id);
+            openedCards.push(card);
         } else if(openedCards.length === 1){
-            openedCards.push(imgToChange.childNodes);
-            openCardsID.push(event.target.id);
+            openedCards.push(card);
 
-            if (openedCards[1][0].src === openedCards[0][0].src){
+            if (openedCards[0].querySelector('img').src === openedCards[1].querySelector('img').src){
                 flipCounter += 2;
                 openedCards = [];
-                openCardsID = [];
 
                 if (flipCounter === shuffledcards.length){
                     alert('Congrats you won');
                 }
             } else {
                 function flipBack() {
-                    var card1 = document.getElementById(openCardsID[0]);
-                    card1.innerHTML = `<i class="icon fas fa-crow fa-7x" id="${ openCardsID[0] }"></i>`;
-
-                    var card2 = document.getElementById(openCardsID[1]);
-                    card2.innerHTML = `<i class="icon fas fa-crow fa-7x" id="${ openCardsID[1] }"></i>`;
+                    openedCards[0].innerHTML = `<i class="icon fas fa-crow fa-7x"></i>`;
+                    openedCards[1].innerHTML = `<i class="icon fas fa-crow fa-7x"></i>`;
 
                     openedCards = [];
-                    openCardsID = [];
                 }
 
                 setTimeout(flipBack, 700);
